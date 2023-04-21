e00537d25d529dc7a1bea614bf564d90bb68efc4
mirabdi <mirabdi@unist.ac.kr>
2023-04-22 02:19:25 +0900

    inspect and notify

--- sync-folders.sh
+++ sync-folders.sh
@@ -1,7 +1,9 @@
 #while true; do 
 #   for f in $(find ../AFsequences -maxdepth 3 -mindepth 3 -type d -mmin -60); do
 #      echo $f
-      rsync -a --include "*/" --include="*pdb" --include="*json" --exclude="*" -e 'ssh -p 50000'  ../AFsequences amirbek@10.70.10.47:/data/Protein
+      src="/home/main/dev/AFsequences"
+      dest="amirbek@10.70.10.47:/data/Protein"
+      rsync -arv -P --include "*/" --include="*pdb" --include="*json" --exclude="*" -e 'ssh -p 50000'  $src $dest
 #   done
 #   sleep 1800
-#done
+done
