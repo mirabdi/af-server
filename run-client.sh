d849d277ead87e6a9a8870517e3436ed7b498e93
John McBride <jmmcbride@protonmail.com>
2023-07-05 14:43:43 +0900

    Added function to server that allows replacing path list

--- run-client.sh
+++ run-client.sh
@@ -7,7 +7,7 @@ while [ -n $seqdir ] ; do
     if [ -n $seqdir ] ; then
        echo "Running colabfold_batch on " $seqdir
        echo "Running colabfold_batch on " $seqdir >> log/client.log
-       colabfold_batch --amber --use-gpu-relax --num-seeds 5 --num-recycle 6  ../AFsequences/$seqdir ../AFsequences/$seqdir 2>> log/client.log
+       colabfold_batch --amber --use-gpu-relax --num-seeds 10 --num-recycle 6  ../AFsequences/$seqdir ../AFsequences/$seqdir 2>> log/client.log
     fi    
     # rsync -a -e 'ssh -p 50000'  ../AFsequences/$seqdir  amirbek@10.70.10.47:/home/protein/Protein/AFsequences/$seqdir
 done
--- server.py
+++ server.py
@@ -14,21 +14,23 @@ import numpy as np
 
 
 
-
+### Creates the initial list of sequences to be served.
+### Run once when starting the server.
 def make_list():
     base = Path("/data/Protein/AFsequences/")
-    main_paths =  ["PDB", "mTag", "PafA", "GFP"]
+    main_paths =  ["ddG"]
     remaining = []
     print("Starting to prepare the list")
     for curr in main_paths:
         current_path = base.joinpath(f"{curr}/AFpred")
         print(f"Looking for folders containing not containing pdb files in AFsequences/{curr}/AFpred") 
         for seqpath in tqdm(current_path.iterdir()):
+            # Sort the files so that they are presented in a predictable manner
             pdb_files = sorted(seqpath.glob('*.pdb'))
             if len(pdb_files) == 0:
                 remaining.append(f"/{curr}/AFpred/{seqpath.name}")
         print(f"Finished adding paths from AF/{curr}!")
-    # remaining = sorted(list(set(remaining)))
+    # Present the list in reverse order, since sequences are drawn from the end of the list
     return remaining[::-1] 
 
 
@@ -69,13 +71,25 @@ class DistributionServer(DistributionBase):
             self.count += 1
             self.load_extra_seq()
 
+
     def load_extra_seq(self):
         seq_path = Path('../extra_seq.txt')
         if seq_path.exists():
-            extra = [l.strip('\n') for l in open(seq_path)]
+            extra = [l.strip('\n') for l in open(seq_path, 'r')]
             os.remove(seq_path)
             self.remaining.extend(extra)
             self.log_extra_seq(extra)
+
+
+    def reload_seq_list(self):
+        seq_path = Path('../reload_seq_list.txt')
+        if seq_path.exists():
+            seq_list = [l.strip('\n') for l in open(seq_path, 'r')]
+            os.remove(seq_path)
+            # Present the list in reverse order, since sequences are drawn from the end of the list
+            self.remaining = seq_list[::-1]
+            self.log_reload_seq(extra)
+
 
     def log_client_request(self, msg, seq):
         with open('log/server.log', 'a') as o:
@@ -83,9 +97,16 @@ class DistributionServer(DistributionBase):
             o.write(f"[{datetime.datetime.now()} ===== Got request, sending the sequence: {seq}\n")
             o.write(f"[{datetime.datetime.now()}] ===== Seqs remaining to fold: {len(self.remaining)}\n")
 
+
     def log_extra_seq(self, extra):
         with open('log/server.log', 'a') as o:
             o.write(f"EXTRA SEQ FOUND: {len(extra)} sequences appended to the front of the queue\n")
+
+
+    def log_reload_seq(self, seq_list):
+        with open('log/server.log', 'a') as o:
+            o.write(f"SEQ LIST RELOADED: {len(seq_list)} sequences found\n")
+
 
 
 if __name__ == '__main__':
